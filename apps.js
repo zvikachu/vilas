db160c4723d6d57af6eedfc1c60ee99cb1888ddc
zvikachu <zviyemin@gmail.com>
2016-08-08 13:27:31 +0300

    itzik var changed

--- apps.js
+++ apps.js
@@ -16,4 +16,6 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
+
+    var itzik = "new itzik var"
 });
